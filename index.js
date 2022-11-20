7b06aa15021faa3637420b42c77c4ba686eab7b1
Bob Oyier <boboyier92@gmail.com>
2022-11-20 19:11:55 +0300

    "completed Moringa School's JavaScript Events lab."

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
-function addingEventListener() {
+/**
+ First grab the element you want to add the listener to.
+ Then create a function that modifies it.
+ This function when called should do the entire process.
+
+ There are two ways of going about it:
+	One is using a defined inner function and another is by
+	using an anonymous inner function.
+
+	The method that uses an anonymous function is commented out
+	below my submitted answer.
+
+	I prefer a defined function because it is cleaner when calling
+	it on the outer function. And also reusable hence potentially
+	less code in future.
+ */
+
+const input = document.getElementById("button");
+
+function clickAlert() {
+	alert("I was clicked!");
 }
+
+function addingEventListener() {
+	input.addEventListener("click", clickAlert);
+}
+
+/**
+	const input = document.getElementById("button");
+
+	function addingEventListener() {
+
+	input.addEventListener("click", function () {
+		alert("I was clicked!");
+	});
+}
+ */
